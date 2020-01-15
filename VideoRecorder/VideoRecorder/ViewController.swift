34783194f36aed16fe1de15c9a88ea77ff6764d3
Jon Bash <jonbash@protonmail.com>
2020-01-15 09:39:05 -0800

    update mainVC

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -18,13 +19,45 @@ class ViewController: UIViewController {
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		// TODO: get permission
-		
-		showCamera()
-		
+		getPermissionAndShowCamera()
 	}
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
 	}
+
+    private func getPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        // skipping over edge cases where things can fail
+        switch status {
+        case .notDetermined:
+            // first time user - they haven't seen the dialog to give permission
+            requestPermission()
+        case .restricted:
+            // Parental controls disabled camera
+            fatalError("Video is disabled for the user (parental controls)")
+            // TODO: add UI to inform the user
+        case .denied:
+            // user did not give us access (maybe it was an accident)
+            fatalError("User denied access to camera")
+            // TODO: add UI to inform the user
+        case .authorized:
+            // we asked for permission (2nd time they've used app)
+            showCamera()
+        @unknown default:
+            fatalError("A new auth status was added that we need to handle")
+        }
+    }
+
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { permissionGranted in
+            guard permissionGranted else {
+                fatalError("User denied access to camera")
+                // TODO: add UI to inform the user
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.showCamera()
+            }
+        }
+    }
 }
